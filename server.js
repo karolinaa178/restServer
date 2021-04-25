bf432a6763e6e04d4a3a67780ab877d0a3e79318
Karolina Okla <karolinaokla95@gmail.com>
2021-04-25 12:03:41 +0200

    Add db.js

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-
-let db = [
-    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-];
+let db = require('./db');
 
 const app = express();
 
@@ -13,33 +9,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/testimonials', (req, res) => {
-  res.json(db);
+  res.json(db.testimonials);
 });
 
 app.get('/testimonials/random', (req, res) => {
-  let item = db[Math.floor(Math.random() * db.length )];
+  let item = db.testimonials[Math.floor(Math.random() * db.length )];
   res.json(item);
 });
 
 app.get('/testimonials/:id', (req, res) => {
-  res.json(db.find(item => item.id == req.params.id));
+  res.json(db.testimonials.find(item => item.id == req.params.id));
 });
 
 
-app.post('/testimonials/', (req, res) => {
+app.post('/testimonials', (req, res) => {
   const obj ={
     id: uuidv4(),
     author: req.body.author,
     text: req.body.text
   }
-  db.push(obj);
+  db.testimonials.push(obj);
   return res.json({
     message: 'ok'
   });
 });
 
 app.put('/testimonials/:id', (req, res) => {
-  db.forEach(element => {
+  db.testimonials.forEach(element => {
     if(element.id == req.params.id) {
       element.author = req.body.author;
       element.text = req.body.text;
@@ -52,10 +48,10 @@ app.put('/testimonials/:id', (req, res) => {
 
 
 app.delete('/testimonials/:id', (req, res) => {
-  db.forEach(element => {
+  db.testimonials.forEach(element => {
     if(element.id == req.params.id) {
-      const index = db.indexOf(element);
-      db.splice(index, 1);
+      const index = db.testimonials.indexOf(element);
+      db.testimonials.splice(index, 1);
     }
   });
   return res.json({
