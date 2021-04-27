b2fb8b8e64a9da53115f3e2f440284d37836f646
Karolina Okla <karolinaokla95@gmail.com>
2021-04-27 17:12:38 +0200

    Add endpoint to public Heroku

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
+
 const app = express();
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 
 app.use(cors());
@@ -16,6 +20,10 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 
 app.use((req, res) => {
   return res.status(404).json({
@@ -24,6 +32,6 @@ app.use((req, res) => {
 });
 
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
